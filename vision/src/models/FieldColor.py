3c6636b6a01e290820ca676eb54a2e7fbdae1b22
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-02-19 17:09:42 -0600

    feat: add FieldColor class for color range detection

---
+++ vision/src/models/FieldColor.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+
+@dataclass
+class FieldColor:
+    """
+    A class that represents a color of the field
+
+    Attributes:
+        color: The color of the field (rgb)
+        color_range: The range of the color of the field (rgb) to be accepted (for each color)
+        name: The name of the color of the field
+    """
+    color: tuple[int, int, int]
+    color_range: tuple[tuple[int, int, int], tuple[int, int, int]]
+    name: str
+
+    def __post_init__(self):
+        self.color_range = (
+            (max(0, self.color[0] - 10), max(0, self.color[1] - 10), max(0, self.color[2] - 10)),
+            (min(255, self.color[0] + 10), min(255, self.color[1] + 10), min(255, self.color[2] + 10))
+        )
+
+    def is_color_in_range(self, color: tuple[int, int, int]) -> bool:
+        """
+        Checks if the color is in the range of the color of the field
+
+        Returns:
+            True if the color is in the range of the color of the field, False otherwise
+        """
+
+        return (
+            self.color_range[0][0] <= color[0] <= self.color_range[1][0] and
+            self.color_range[0][1] <= color[1] <= self.color_range[1][1] and
+            self.color_range[0][2] <= color[2] <= self.color_range[1][2]
+        )
